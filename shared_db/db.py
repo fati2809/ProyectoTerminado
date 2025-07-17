b88dc12c0ed4629961e82ac3c8243bcfbcc4e2ee
ManuelSMC <ManuelSMC>
2025-07-17 16:14:27 -0600

    MFA - Multi Factor Authenticator

--- shared_db/db.py
+++ shared_db/db.py
@@ -11,21 +11,26 @@ def get_db_connection() -> sqlite3.Connection:
     return conn
 
 def init_db():
-    """Initializes the shared database schema only if needed."""
+    """Initializes the shared database schema and adds MFA fields if needed."""
     base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
     db_path = os.path.join(base_dir, 'shared_db', 'database.db')
     conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
+
+    # Crear tabla users si no existe
     cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='users'")
     if not cursor.fetchone():
         cursor.execute("""
-            CREATE TABLE users (
+            CREATE TABLE IF NOT EXISTS users(
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
-                username TEXT NOT NULL,
+                username TEXT NOT NULL UNIQUE,
                 password TEXT NOT NULL,
-                status INTEGER DEFAULT 1
+                status INTEGER DEFAULT 1,
+                two_factor_secret TEXT,
+                two_factor_enabled BOOLEAN DEFAULT FALSE
             )
         """)
+        # Insertar usuarios de prueba
         users = [
             ('username1', 'Hola.123', 1),
             ('username2', 'Hola.123', 1),
@@ -35,8 +40,17 @@ def init_db():
         for user in users:
             username, password, status = user
             cursor.execute(
-                "INSERT OR IGNORE INTO users (username, password, status) VALUES (?, ?, ?)",
-                (username, generate_password_hash(password), status)
+                "INSERT OR IGNORE INTO users (username, password, status, two_factor_enabled) VALUES (?, ?, ?, ?)",
+                (username, generate_password_hash(password), status, False)
             )
+    else:
+        # Verificar y agregar columnas solo si no existen
+        cursor.execute("PRAGMA table_info(users)")
+        columns = {info[1] for info in cursor.fetchall()}  # Conjunto de nombres de columnas
+        if 'two_factor_secret' not in columns:
+            cursor.execute("ALTER TABLE users ADD COLUMN two_factor_secret TEXT")
+        if 'two_factor_enabled' not in columns:
+            cursor.execute("ALTER TABLE users ADD COLUMN two_factor_enabled BOOLEAN DEFAULT FALSE")
+
     conn.commit()
     conn.close()
